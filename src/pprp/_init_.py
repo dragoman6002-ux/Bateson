a4686f17da9d4dd73f24a7f6ae983354b60e6c9a
dragoman6002-ux <dragoman6002@gmail.com>
2025-10-17 13:22:26 -0400

    Add PPRP module Imports
    Added initial implementation of the PPRP framework with versioning and author information.

---
+++ src/pprp/_init_.py
@@ -0,0 +1,23 @@
+"""
+Pi-Phi-Prime Resonant Information Processor (PPRP)
+
+A mathematical information processing framework combining π, φ, and prime numbers.
+"""
+
+__version__ = "0.1.0"
+__author__ = "Daniel Dragolich"
+
+# Import main classes
+from .core import EnhancedPiCore, EnhancedPhiCore, EnhancedOmegaCore, EnhancedBetaCore
+from .manifold import EnhancedSubstrateManifold
+from .nodule import EnhancedPrimeNodule
+from .operators import EnhancedTransputation, EnhancedRealitySelection, EnhancedAwareness
+from .system_api import EnhancedCGOSSyscall
+from .options import PPRIPOptions
+
+__all__ = [
+    'EnhancedPiCore', 'EnhancedPhiCore', 'EnhancedOmegaCore', 'EnhancedBetaCore',
+    'EnhancedSubstrateManifold', 'EnhancedPrimeNodule',
+    'EnhancedTransputation', 'EnhancedRealitySelection', 'EnhancedAwareness',
+    'EnhancedCGOSSyscall', 'PPRIPOptions'
+]

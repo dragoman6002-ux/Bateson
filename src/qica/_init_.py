9aad05218b6855a6a114c8bd2acd86164500d641
dragoman6002-ux <dragoman6002@gmail.com>
2025-10-17 13:32:05 -0400

    Add __init__.py to qica directory

---
+++ src/qica/_init_.py
@@ -0,0 +1,22 @@
+"""
+Quantum-Information Consciousness Architecture (QICA)
+
+Framework for simulating emergent computation through dynamic thresholds
+and self-referential processing.
+"""
+
+__version__ = "0.1.0"
+
+from .engine import EnhancedQICAEngine
+from .field import EnhancedConsciousnessField
+from .constants import EnhancedConsciousnessConstants
+from .states import EnhancedConsciousnessState
+from .memory import ConsciousnessMemory
+from .self_reference import SelfReferenceEngine
+from .threshold_controller import DynamicThresholdController
+
+__all__ = [
+    'EnhancedQICAEngine', 'EnhancedConsciousnessField', 'EnhancedConsciousnessConstants',
+    'EnhancedConsciousnessState', 'ConsciousnessMemory', 'SelfReferenceEngine',
+    'DynamicThresholdController'
+]
